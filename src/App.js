a8e307921fb3c156f312288f7aa266129082b58e
Work Drive <workdrive0110@gmail.com>
2020-02-23 13:56:50 +0545

    adding files in github

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+import './App.css';
+
+class App extends Component{
+  constructor(){
+    super();
+
+  this.state = {
+    monsters: [],
+    searchField: ''
+  };
+
+  this.handleChange=this.handleChange.bind(this);
+} 
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({monsters: users}));
+}
+
+handleChange(e){
+  this.setState({ searchField: e.target.value})
+}
+
+render(){
+
+  const { monsters, searchField } = this.state;
+  const filteredMonsters = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
+  return (
+    <div className="App">
+      <h1>Robot Heads</h1>
+      <SearchBox
+      placeholder='Search Heredes'
+      handleChange={this.handleChange}
+      />
+
+      <CardList monsters={filteredMonsters}>
+      </CardList>
+    </div>
+  );
+}
+}
+
+export default App;
