c770956d3ae2c7ab07a081e6aabfed59b1afc362
Work Drive <workdrive0110@gmail.com>
2020-02-23 13:55:44 +0545

    Updates

---
+++ precache-manifest.0ae2fc9d6efa816de0bb53632091d757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bb1028c2d06a598bd25110edcb7212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "babe98208c873cb95fa6",
+    "url": "/static/css/main.0acd829d.chunk.css"
+  },
+  {
+    "revision": "5972ab9a24d87de2b217",
+    "url": "/static/js/2.40aee04e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.40aee04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babe98208c873cb95fa6",
+    "url": "/static/js/main.b158d67c.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  }
+]);
